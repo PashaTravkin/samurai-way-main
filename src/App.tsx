7f5d99fefa95cf58751c6e65bd36f6d7c31bbbe2
PashaTravkin <refueller2022@bk.ru>
2022-05-22 22:25:54 +0300

    structure site

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,14 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
 import {Nav} from "./components/Nav";
+import {Profile} from "./components/Profile";
 
 function App() {
     return (
         <div className="app-wraper">
-           <Header/>
+            <Header/>
             <Nav/>
-            <
-
-            < div className="content">
-                <div className="mainImgInContent"><img
-                          src="https://besthqwallpapers.com/Uploads/31-3-2021/160338/thumb2-social-networks-blue-background-social-networks-icons-blue-light-globe-global-networks.jpg"
-                          alt=""/></div>
-                <div className='workingArea' >
-                    <div>ava+description</div>
-                    <div>My post</div>
-                    <div>New post</div>
-                    <div>post1</div>
-                    <div>post2</div>
-                </div>
-            </div>
+            <Profile/>
 
 
         </div>
---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+export let Profile = ()=>{
+    return(
+        < div className="content">
+            <div className="mainImgInContent"><img
+                src="https://besthqwallpapers.com/Uploads/31-3-2021/160338/thumb2-social-networks-blue-background-social-networks-icons-blue-light-globe-global-networks.jpg"
+                alt=""/></div>
+            <div className='workingArea' >
+                <div>ava+description</div>
+                <div>My post</div>
+                <div>New post</div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    )
+}
