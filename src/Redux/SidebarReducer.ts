3f8e251af53f060f3d81361a78fb1a135038d596
PashaTravkin <refueller2022@bk.ru>
2022-07-25 02:15:40 +0300

    creat SidebarReducer

---
+++ src/Redux/SidebarReducer.ts
@@ -0,0 +1,7 @@
+import {ActionsType, sidebarType} from "./State";
+
+const SidebarReducer = (sidebarState:Array<sidebarType>, action:ActionsType) => {
+    return sidebarState
+};
+
+export default SidebarReducer;
