b84cf46a9edba7d564f676d9e2a6ae83b30f7c75
PashaTravkin <refueller2022@bk.ru>
2022-10-07 17:50:30 +0300

    create api.tsx => DSL

---
+++ src/api/api.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    headers: {"API-KEY": "f01b381b-4376-4ff2-8452-c680d96141e5"}
+})
+
+export let usersAPI={
+    getUsers (currentPage: number = 1, pageSize: number = 10) {
+        return (
+            instance.get(`users?page=${currentPage}&count=${pageSize}`)
+                .then(response=>response.data)
+        )
+    },
+
+    setFollowed (id:number){
+        return(
+            instance.post(`follow/${id}`)
+                .then(response=>response.data)
+        )
+    },
+
+    setUnfollowed (id:number){
+        return(
+            instance.delete(`follow/${id}`)
+                .then(response=>response.data)
+        )
+    },
+
+
+}
+
